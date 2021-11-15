15288570dfbc8440d4da0cd15644a9c5f81a78cc
Linda2734 <94357533+Linda2734@users.noreply.github.com>
2021-11-15 19:25:19 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var ball;
+function setup() {
+  createCanvas(400,400);
+  ball = createSprite(200,200,10,10);
+}
+
+function draw() 
+{
+  background(30);
+
+  if (keyDown("left")){
+    ball.x -= 2;
+  }
+
+  if (keyDown("right")){
+    ball.x += 2;
+  }
+drawSprites();
+}
+
+
+
+
